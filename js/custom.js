9330e435c9dc41bbe8ddc9b45e40925748156669
chhyyi <chhyyi@gmail.com>
2023-09-10 15:20:32 +0900

    two particle effect works

--- js/custom.js
+++ js/custom.js
@@ -185,120 +185,137 @@ $(window).on("load", function() {
   };
 
   // Particles BG - particlesJS
+  //
+  const particles={
+    "number": {
+      "value": 220,
+      "density": {
+        "enable": true,
+        "value_area": 800
+        
+      }
+    },
+    "color": {
+      "value": "000000"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0.1,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 1,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 4,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 60,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 180,
+      "color": "#e83845",
+      "opacity": 0.3,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 2,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  }
+  const interactivity={
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": false,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 800,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 800,
+        "size": 80,
+        "duration": 2,
+        "opacity": 0.8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 100,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  }
   var bgndParticles = $('#particles-js');
   if (bgndParticles.length) {
     particlesJS('particles-js', {
-        "particles": {
-          "number": {
-            "value": 220,
-            "density": {
-              "enable": true,
-              "value_area": 800
-              
-            }
-          },
-          "color": {
-            "value": "000000"
-          },
-          "shape": {
-            "type": "circle",
-            "stroke": {
-              "width": 0.1,
-              "color": "#000000"
-            },
-            "polygon": {
-              "nb_sides": 5
-            },
-            "image": {
-              "src": "img/github.svg",
-              "width": 100,
-              "height": 100
-            }
-          },
-          "opacity": {
-            "value": 1,
-            "random": true,
-            "anim": {
-              "enable": true,
-              "speed": 1,
-              "opacity_min": 0.1,
-              "sync": false
-            }
-          },
-          "size": {
-            "value": 4,
-            "random": true,
-            "anim": {
-              "enable": false,
-              "speed": 60,
-              "size_min": 0.1,
-              "sync": false
-            }
-          },
-          "line_linked": {
-            "enable": true,
-            "distance": 180,
-            "color": "#e83845",
-            "opacity": 0.3,
-            "width": 1
-          },
-          "move": {
-            "enable": true,
-            "speed": 2,
-            "direction": "none",
-            "random": false,
-            "straight": false,
-            "out_mode": "out",
-            "bounce": false,
-            "attract": {
-              "enable": false,
-              "rotateX": 600,
-              "rotateY": 1200
-            }
-          }
-        },
-        "interactivity": {
-          "detect_on": "canvas",
-          "events": {
-            "onhover": {
-              "enable": true,
-              "mode": "repulse"
-            },
-            "onclick": {
-              "enable": false,
-              "mode": "push"
-            },
-            "resize": true
-          },
-          "modes": {
-            "grab": {
-              "distance": 800,
-              "line_linked": {
-                "opacity": 1
-              }
-            },
-            "bubble": {
-              "distance": 800,
-              "size": 80,
-              "duration": 2,
-              "opacity": 0.8,
-              "speed": 3
-            },
-            "repulse": {
-              "distance": 100,
-              "duration": 0.4
-            },
-            "push": {
-              "particles_nb": 4
-            },
-            "remove": {
-              "particles_nb": 2
-            }
-          }
-        },
+        "particles": particles
+        ,
+        "interactivity": interactivity
+        ,
         "retina_detect": true
       });
   };
+  var bgndParticles2 = $('#particles2-js');
+  if (bgndParticles2.length) {
+    particlesJS('particles2-js', {
+        "particles": particles
+        ,
+        "interactivity": interactivity
+        ,
+        "retina_detect": true
+      });
+  };
+
+
   // --------------------------------------------- //
   // ParticlesJS Backgrounds End
   // --------------------------------------------- //
